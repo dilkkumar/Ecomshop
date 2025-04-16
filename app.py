96887abb6f2bc9a9333518dfe683b881b8028ebc
dilkkumar <109529270+dilkkumar@users.noreply.github.com>
2025-04-16 11:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -289,4 +289,4 @@ def logout():
         pass
     return redirect(url_for('login'))  # Redirect after logout
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
